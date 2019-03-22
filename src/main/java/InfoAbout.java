b2ead9fa5c07604de734ac462b0bbfd73cd19fc3
Szymon Kwasiborski <s_kwasiborski@wp.pl>
2019-03-22 15:00:03 +0100

    finished calc,except jar and exe

---
+++ src/main/java/InfoAbout.java
@@ -0,0 +1,15 @@
+import javax.swing.*;
+
+public class InfoAbout {
+    public JPanel infoPanel;
+
+    public static void main(String[] args) {
+
+        JFrame infoFrame = new JFrame("InfoAbout");
+        infoFrame.setContentPane(new InfoAbout().infoPanel);
+        infoFrame.setDefaultCloseOperation(JFrame.HIDE_ON_CLOSE);
+        infoFrame.pack();
+        infoFrame.setResizable(false);
+        infoFrame.setVisible(true);
+    }
+}
